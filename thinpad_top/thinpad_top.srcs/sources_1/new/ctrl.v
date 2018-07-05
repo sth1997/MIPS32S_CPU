15144487f99cd7f69eec6d0c5263e949f789ae0e
sth1997 <765215342@qq.com>
2018-07-06 00:28:45 +0800

    Add ctrl.

---
+++ thinpad_top/thinpad_top.srcs/sources_1/new/ctrl.v
@@ -0,0 +1,42 @@
+`include "defines.v"
+
+module ctrl(
+
+	input wire                rst,
+	input wire                stallreq_from_id,
+	input wire                stallreq_from_if,
+	input wire                stallreq_from_mem,
+
+	output reg[`RegBus] new_pc,
+	output reg flush,
+
+	output reg[5: 0]           stall       
+	
+);
+
+	always @ (*) begin
+		if(rst == `RstEnable) begin
+			stall <= 6'b000000;
+			flush <= 1'b0;
+			new_pc <= `ZeroWord;
+		end 
+		else if(stallreq_from_mem == `Stop) begin
+			stall <= 6'b011111;		
+			flush <= 1'b0;	
+		end 
+		else if(stallreq_from_id == `Stop) begin
+			stall <= 6'b000111;		
+			flush <= 1'b0;	
+		end 
+		else if(stallreq_from_if == `Stop) begin
+			stall <= 6'b000111;	// stall the id_stage in order to keep the order of branch_inst and inst_in_delayslot
+			flush <= 1'b0;	
+		end 
+		else begin
+			stall <= 6'b000000;
+			flush <= 1'b0;
+			new_pc <= `ZeroWord;
+		end    
+	end     		
+
+endmodule
