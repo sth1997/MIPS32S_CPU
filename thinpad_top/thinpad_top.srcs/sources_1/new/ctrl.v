26af6768e0a963176a6bcebf451d787bc10ecd0d
chenqiuhao <haohao0924@126.com>
2018-07-12 16:14:42 +0800

    ctrl update

--- thinpad_top/thinpad_top.srcs/sources_1/new/ctrl.v
+++ thinpad_top/thinpad_top.srcs/sources_1/new/ctrl.v
@@ -6,7 +6,10 @@ module ctrl(
 	input wire                stallreq_from_id,
 	input wire                stallreq_from_if,
 	input wire                stallreq_from_mem,
-
+	
+	input wire[31: 0] excepttype_i,
+	input wire[`RegBus] cp0_epc_i,
+	input wire[`RegBus] exc_vec_addr_i, // exception vector address
 	output reg[`RegBus] new_pc,
 	output reg flush,
 
@@ -20,6 +23,38 @@ module ctrl(
 			flush <= 1'b0;
 			new_pc <= `ZeroWord;
 		end 
+		else if(excepttype_i != `ZeroWord) begin
+				flush <= 1'b1;
+				stall <= 6'b000000;
+				case (excepttype_i)
+					32'h00000001: begin  // interruption
+						new_pc <= exc_vec_addr_i + 32'h180;
+					end
+					32'h00000008: begin  // syscall
+						new_pc <= exc_vec_addr_i + 32'h180;
+					end
+					32'h0000000a: begin  // RI
+						new_pc <= exc_vec_addr_i + 32'h180;
+					end
+					32'h0000000b: begin  // ADES
+						new_pc <= exc_vec_addr_i + 32'h180;
+					end
+					32'h0000000c: begin  // ADEL
+						new_pc <= exc_vec_addr_i + 32'h180;
+					end
+					32'h0000000e: begin  // eret
+						new_pc <= cp0_epc_i;
+					end
+					/*
+					32'h0000000f, 32'h00000010, 32'h00000011, 32'h00000012: begin  
+					// TLBL, inst; TLB Mod;      TLBL_Data;    TLBS
+						new_pc <= exc_vec_addr_i;
+					end
+					*/
+					default : begin 
+					end
+				endcase
+		end
 		else if(stallreq_from_mem == `Stop) begin
 			stall <= 6'b011111;		
 			flush <= 1'b0;	
