5a04ae2390c591d59680450a47ac52ebd4200092
sth1997 <765215342@qq.com>
2018-07-19 16:15:47 +0800

    Add flash module and add it into thinpad_top.

--- thinpad_top/thinpad_top.srcs/sources_1/new/thinpad_top.v
+++ thinpad_top/thinpad_top.srcs/sources_1/new/thinpad_top.v
@@ -4,51 +4,51 @@ module thinpad_top(
     input wire clk_50M,           //50MHz 时钟输入
     input wire clk_11M0592,       //11.0592MHz 时钟输入
 
-    input wire clock_btn,         //BTN5手动时钟按钮�?关，带消抖电路，按下时为1
-    input wire reset_btn,         //BTN6手动复位按钮�?关，带消抖电路，按下时为1
+    input wire clock_btn,         //BTN5手动时钟按钮�??关，带消抖电路，按下时为1
+    input wire reset_btn,         //BTN6手动复位按钮�??关，带消抖电路，按下时为1
 
     input  wire[3:0]  touch_btn,  //BTN1~BTN4，按钮开关，按下时为1
-    input  wire[31:0] dip_sw,     //32位拨码开关，拨到“ON”时�?1
+    input  wire[31:0] dip_sw,     //32位拨码开关，拨到“ON”时�??1
     output wire[15:0] leds,       //16位LED，输出时1点亮
     output wire[7:0]  dpy0,       //数码管低位信号，包括小数点，输出1点亮
     output wire[7:0]  dpy1,       //数码管高位信号，包括小数点，输出1点亮
 
-    //CPLD串口控制器信�?
-    output wire uart_rdn,         //读串口信号，低有�?
-    output wire uart_wrn,         //写串口信号，低有�?
-    input wire uart_dataready,    //串口数据准备�?
-    input wire uart_tbre,         //发�?�数据标�?
-    input wire uart_tsre,         //数据发�?�完毕标�?
+    //CPLD串口控制器信�??
+    output wire uart_rdn,         //读串口信号，低有�??
+    output wire uart_wrn,         //写串口信号，低有�??
+    input wire uart_dataready,    //串口数据准备�??
+    input wire uart_tbre,         //发�?�数据标�??
+    input wire uart_tsre,         //数据发�?�完毕标�??
 
     //BaseRAM信号
-    inout wire[31:0] base_ram_data,  //BaseRAM数据，低8位与CPLD串口控制器共�?
+    inout wire[31:0] base_ram_data,  //BaseRAM数据，低8位与CPLD串口控制器共�??
     output wire[19:0] base_ram_addr, //BaseRAM地址
-    output wire[3:0] base_ram_be_n,  //BaseRAM字节使能，低有效。如果不使用字节使能，请保持�?0
-    output wire base_ram_ce_n,       //BaseRAM片�?�，低有�?
-    output wire base_ram_oe_n,       //BaseRAM读使能，低有�?
-    output wire base_ram_we_n,       //BaseRAM写使能，低有�?
+    output wire[3:0] base_ram_be_n,  //BaseRAM字节使能，低有效。如果不使用字节使能，请保持�??0
+    output wire base_ram_ce_n,       //BaseRAM片�?�，低有�??
+    output wire base_ram_oe_n,       //BaseRAM读使能，低有�??
+    output wire base_ram_we_n,       //BaseRAM写使能，低有�??
 
     //ExtRAM信号
     inout wire[31:0] ext_ram_data,  //ExtRAM数据
     output wire[19:0] ext_ram_addr, //ExtRAM地址
-    output wire[3:0] ext_ram_be_n,  //ExtRAM字节使能，低有效。如果不使用字节使能，请保持�?0
-    output wire ext_ram_ce_n,       //ExtRAM片�?�，低有�?
-    output wire ext_ram_oe_n,       //ExtRAM读使能，低有�?
-    output wire ext_ram_we_n,       //ExtRAM写使能，低有�?
+    output wire[3:0] ext_ram_be_n,  //ExtRAM字节使能，低有效。如果不使用字节使能，请保持�??0
+    output wire ext_ram_ce_n,       //ExtRAM片�?�，低有�??
+    output wire ext_ram_oe_n,       //ExtRAM读使能，低有�??
+    output wire ext_ram_we_n,       //ExtRAM写使能，低有�??
 
     //直连串口信号
     output wire txd,  //直连串口发�?�端
-    input  wire rxd,  //直连串口接收�?
+    input  wire rxd,  //直连串口接收�??
 
     //Flash存储器信号，参�?? JS28F640 芯片手册
-    output wire [22:0]flash_a,      //Flash地址，a0仅在8bit模式有效�?16bit模式无意�?
+    output wire [22:0]flash_a,      //Flash地址，a0仅在8bit模式有效�??16bit模式无意�??
     inout  wire [15:0]flash_d,      //Flash数据
     output wire flash_rp_n,         //Flash复位信号，低有效
-    output wire flash_vpen,         //Flash写保护信号，低电平时不能擦除、烧�?
-    output wire flash_ce_n,         //Flash片�?�信号，低有�?
-    output wire flash_oe_n,         //Flash读使能信号，低有�?
-    output wire flash_we_n,         //Flash写使能信号，低有�?
-    output wire flash_byte_n,       //Flash 8bit模式选择，低有效。在使用flash�?16位模式时请设�?1
+    output wire flash_vpen,         //Flash写保护信号，低电平时不能擦除、烧�??
+    output wire flash_ce_n,         //Flash片�?�信号，低有�??
+    output wire flash_oe_n,         //Flash读使能信号，低有�??
+    output wire flash_we_n,         //Flash写使能信号，低有�??
+    output wire flash_byte_n,       //Flash 8bit模式选择，低有效。在使用flash�??16位模式时请设�??1
 
     //USB 控制器信号，参�?? SL811 芯片手册
     output wire sl811_a0,
@@ -71,13 +71,13 @@ module thinpad_top(
     input  wire dm9k_int,
 
     //图像输出信号
-    output wire[2:0] video_red,    //红色像素�?3�?
-    output wire[2:0] video_green,  //绿色像素�?3�?
-    output wire[1:0] video_blue,   //蓝色像素�?2�?
-    output wire video_hsync,       //行同步（水平同步）信�?
-    output wire video_vsync,       //场同步（垂直同步）信�?
+    output wire[2:0] video_red,    //红色像素�??3�??
+    output wire[2:0] video_green,  //绿色像素�??3�??
+    output wire[1:0] video_blue,   //蓝色像素�??2�??
+    output wire video_hsync,       //行同步（水平同步）信�??
+    output wire video_vsync,       //场同步（垂直同步）信�??
     output wire video_clk,         //像素时钟输出
-    output wire video_de           //行数据有效信号，用于区分消隐�?
+    output wire video_de           //行数据有效信号，用于区分消隐�??
 );
 
     wire rst;
@@ -262,6 +262,40 @@ SERIAL serial0(
         
         .dataready(uart_int)
 );
+
+        // flash wb
+        wire [31:0] flash_data_i;
+        wire [31:0] flash_data_o;
+        wire [31:0] flash_addr_o;
+        wire [3:0] flash_sel_o;
+        wire flash_we_o;
+        wire flash_cyc_o;
+        wire flash_stb_o;
+        wire flash_ack_i;
+    
+flash flash0(
+        .wb_clk_i(clk_o),
+        .wb_rst_i(rst), 
+        .wb_adr_i(flash_addr_o), 
+        .wb_dat_o(flash_data_i), 
+        .wb_dat_i(flash_data_o), 
+        .wb_sel_i(flash_sel_o), 
+        .wb_we_i(flash_we_o),
+        .wb_stb_i(flash_stb_o), 
+        .wb_cyc_i(flash_cyc_o), 
+        .wb_ack_o(flash_ack_i),
+        
+        .flash_adr_o(flash_a), 
+        .flash_dat_io(flash_d), 
+        .flash_byte(flash_byte_n),
+        .flash_ce(flash_ce_n),
+        .flash_oe(flash_oe_n),
+        .flash_rp(flash_rp_n),
+        .flash_vpen(flash_vpen),
+        .flash_we(flash_we_n)
+);
+
+
 //wb_conmax
 wb_conmax_top wb_conmax_top0(
     .clk_i(clk_o), .rst_i(rst),
@@ -395,15 +429,15 @@ wb_conmax_top wb_conmax_top0(
 
     // Slave 1 Interface: flash
 
-    .s1_data_i(), 
-    .s1_data_o(), 
-    .s1_addr_o(), 
-    .s1_sel_o(), 
-    .s1_we_o(), 
-    .s1_cyc_o(),
+    .s1_data_i(flash_data_i), 
+    .s1_data_o(flash_data_o), 
+    .s1_addr_o(flash_addr_o), 
+    .s1_sel_o(flash_sel_o), 
+    .s1_we_o(flash_we_o), 
+    .s1_cyc_o(flash_cyc_o),
     
-    .s1_stb_o(), 
-    .s1_ack_i(1'b0), 
+    .s1_stb_o(flash_stb_o), 
+    .s1_ack_i(flash_ack_i), 
     .s1_err_i(1'b0), 
     .s1_rty_i(1'b0),
 
@@ -619,7 +653,7 @@ wb_conmax_top wb_conmax_top0(
 // g=dpy0[7] // |     |
 //           // ---d---  p
 
-// 7段数码管译码器演示，将number�?16进制显示在数码管上面
+// 7段数码管译码器演示，将number�??16进制显示在数码管上面
 reg[7:0] number;
 SEG7_LUT segL(.oSEG1(dpy0), .iDIG(number[3:0])); //dpy0是低位数码管
 SEG7_LUT segH(.oSEG1(dpy1), .iDIG(number[7:4])); //dpy1是高位数码管
@@ -638,19 +672,19 @@ always@(posedge clock_btn or posedge reset_btn) begin
     end
 end
 
-//直连串口接收发�?�演示，从直连串口收到的数据再发送出�?
+//直连串口接收发�?�演示，从直连串口收到的数据再发送出�??
 wire [7:0] ext_uart_rx;
 reg  [7:0] ext_uart_buffer, ext_uart_tx;
 wire ext_uart_ready, ext_uart_busy;
 reg ext_uart_start, ext_uart_avai;
 
-async_receiver #(.ClkFrequency(50000000),.Baud(9600)) //接收模块�?9600无检验位
+async_receiver #(.ClkFrequency(50000000),.Baud(9600)) //接收模块�??9600无检验位
     ext_uart_r(
         .clk(clk_50M),                       //外部时钟信号
         .RxD(rxd),                           //外部串行信号输入
-        .RxD_data_ready(ext_uart_ready),  //数据接收到标�?
+        .RxD_data_ready(ext_uart_ready),  //数据接收到标�??
         .RxD_clear(ext_uart_ready),       //清除接收标志
-        .RxD_data(ext_uart_rx)             //接收到的�?字节数据
+        .RxD_data(ext_uart_rx)             //接收到的�??字节数据
     );
     
 always @(posedge clk_50M) begin //接收到缓冲区ext_uart_buffer
@@ -661,7 +695,7 @@ always @(posedge clk_50M) begin //接收到缓冲区ext_uart_buffer
         ext_uart_avai <= 0;
     end
 end
-always @(posedge clk_50M) begin //将缓冲区ext_uart_buffer发�?�出�?
+always @(posedge clk_50M) begin //将缓冲区ext_uart_buffer发�?�出�??
     if(!ext_uart_busy && ext_uart_avai)begin 
         ext_uart_tx <= ext_uart_buffer;
         ext_uart_start <= 1;
@@ -674,8 +708,8 @@ async_transmitter #(.ClkFrequency(50000000),.Baud(9600)) //发�?�模块，96
     ext_uart_t(
         .clk(clk_50M),                  //外部时钟信号
         .TxD(txd),                      //串行信号输出
-        .TxD_busy(ext_uart_busy),       //发�?�器忙状态指�?
-        .TxD_start(ext_uart_start),    //�?始发送信�?
+        .TxD_busy(ext_uart_busy),       //发�?�器忙状态指�??
+        .TxD_start(ext_uart_start),    //�??始发送信�??
         .TxD_data(ext_uart_tx)        //待发送的数据
     );
 
@@ -687,8 +721,8 @@ assign video_blue = hdata >= 532 ? 2'b11 : 0; //蓝色竖条
 assign video_clk = clk_50M;
 vga #(12, 800, 856, 976, 1040, 600, 637, 643, 666, 1, 1) vga800x600at75 (
     .clk(clk_50M), 
-    .hdata(hdata), //横坐�?
-    .vdata(),      //纵坐�?
+    .hdata(hdata), //横坐�??
+    .vdata(),      //纵坐�??
     .hsync(video_hsync),
     .vsync(video_vsync),
     .data_enable(video_de)
